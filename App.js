d8227eddf38eb044ff147fba78dab681286ed0e6
Wagner <81321824+roninroxin@users.noreply.github.com>
2024-04-01 03:47:45 -0300

    Update App.js
    Ajeitei o erro do app

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import HomeScreen from "./src/screens/Home/index";
 import Login from "./src/screens/Login/index";
 import Register from "./src/screens/Register/index";
 import { Gastos } from "./src/screens/Gastos";
